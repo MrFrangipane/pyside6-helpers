4f5b297683efc39b988196ce7301ee14bad0a287
MrFrangipane <contact@frangipane.me>
2023-12-13 11:00:40 +0100

    imp(message box): multiline argument list[str]

--- pyside6helpers/message_box.py
+++ pyside6helpers/message_box.py
@@ -1,39 +1,48 @@
 from PySide6.QtWidgets import QApplication, QMessageBox
 
 
-def critical_box(message: str) -> None:
-    """Show a critical QMessageBox with Ok buttons"""
+def critical_box(message_lines: list[str]) -> None:
+    """Show a critical QMessageBox with Ok button"""
     _exec_box(
         icon=QMessageBox.Icon.Critical,
         title="Critical",
-        message=message
+        message_lines=message_lines
     )
 
 
-def information_box(message: str) -> None:
-    """Show an information QMessageBox with Ok buttons"""
+def warning_box(message_lines: list[str]) -> None:
+    """Show a warning QMessageBox with Ok button"""
+    _exec_box(
+        icon=QMessageBox.Icon.Warning,
+        title="Information",
+        message_lines=message_lines
+    )
+
+
+def information_box(message_lines: list[str]) -> None:
+    """Show an information QMessageBox with Ok button"""
     _exec_box(
         icon=QMessageBox.Icon.Information,
         title="Information",
-        message=message
+        message_lines=message_lines
     )
 
 
-def confirmation_box(message: str) -> bool:
+def confirmation_box(message_lines: list[str]) -> bool:
     """Show a confirmation QMessageBox with Ok & Cancel buttons"""
     return _exec_box(
         icon=QMessageBox.Icon.Question,
         title="Are you sure ?",
-        message=message,
+        message_lines=message_lines,
         has_cancel=True
     )
 
 
-def _exec_box(icon, title, message, has_cancel=False) -> bool:
+def _exec_box(icon: QMessageBox.Icon, title: str, message_lines: list[str], has_cancel=False) -> bool:
     _box = QMessageBox(QApplication.activeWindow())
     _box.setIcon(icon)
     _box.setWindowTitle(title)
-    _box.setText(message)
+    _box.setText("\n".join(message_lines))
     if has_cancel:
         _box.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
     else:
