7b9b2eb53b3ddacd77b2758dd7f890fe2cc4c7c0
MrFrangipane <contact@frangipane.me>
2024-02-04 16:52:26 +0100

    feat:(layout): clear

---
+++ pyside6helpers/layout.py
@@ -0,0 +1,11 @@
+
+
+def clear(layout):
+    if layout is not None:
+        while layout.count():
+            item = layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.deleteLater()
+            else:
+                clear(item.layout())
