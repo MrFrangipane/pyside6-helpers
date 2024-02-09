d180744ddda0c865e74514470ff79f544c3cd895
MrFrangipane <contact@frangipane.me>
2024-02-09 12:07:40 +0100

    misc: added whitelines

--- pyside6helpers/logger/__init__.py
+++ pyside6helpers/logger/__init__.py
@@ -6,9 +6,11 @@ from pyside6helpers.logger.widget import LoggerWidget
 
 def dock_logger_to_main_window(main_window: QMainWindow):
     logger_widget = LoggerWidget()
+
     logger_dock_widget = QDockWidget()
     logger_dock_widget.setWindowTitle("Logger")
     logger_dock_widget.setFeatures(QDockWidget.DockWidgetFeature.NoDockWidgetFeatures)
     logger_dock_widget.setAllowedAreas(Qt.DockWidgetArea.BottomDockWidgetArea)
     logger_dock_widget.setWidget(logger_widget)
+
     main_window.addDockWidget(Qt.DockWidgetArea.BottomDockWidgetArea, logger_dock_widget)
