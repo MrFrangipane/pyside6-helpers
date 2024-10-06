c6fbc5a1ced15a7e497aa4003a494aa9c001a800
MrFrangipane <contact@frangipane.me>
2024-10-07 00:31:33 +0200

    feat: filepath for CSS loading + use pyproject.tomlinstead of setup.py

--- pyside6helpers/css/__init__.py
+++ pyside6helpers/css/__init__.py
@@ -10,6 +10,9 @@ def _make_resource_filepath(filename):
     return os.path.join(os.path.dirname(_HERE), "resources", filename)
 
 
-def load_onto(q_widget: QWidget):
-    with open(_make_resource_filepath("dark.qss"), "r") as stylesheet_file:
+def load_onto(q_widget: QWidget, filepath=None):
+    if filepath is None:
+        filepath = _make_resource_filepath("dark.qss")
+
+    with open(filepath, "r") as stylesheet_file:
         q_widget.setStyleSheet(stylesheet_file.read())
