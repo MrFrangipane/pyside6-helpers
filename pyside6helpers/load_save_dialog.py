f982beb872225275f53fde7877c88facf9093863
MrFrangipane <contact@frangipane.me>
2024-10-07 15:56:42 +0200

    feat(LoadSaveDialog): add parent argument

--- pyside6helpers/load_save_dialog.py
+++ pyside6helpers/load_save_dialog.py
@@ -1,22 +1,22 @@
 from PySide6.QtWidgets import QFileDialog
 
 
-def make_save_hook(title, name_filter, working_directory, callback):
+def make_save_hook(title, name_filter, working_directory, callback, parent=None):
     """Returns a callable that will getSaveFileName() and calls callback() if a file has been chosen"""
     def save_hook(self):
         dialog = QFileDialog()
-        filepath, extension = dialog.getSaveFileName(None, title, working_directory, name_filter)
+        filepath, extension = dialog.getSaveFileName(parent, title, working_directory, name_filter)
         if filepath:
             callback(filepath)
 
     return save_hook
 
 
-def make_open_hook(title, name_filter, working_directory, callback):
+def make_open_hook(title, name_filter, working_directory, callback, parent=None):
     """Returns a callable that will getOpenFileName() and calls callback() if a file has been chosen"""
     def save_hook(self):
         dialog = QFileDialog()
-        filepath, extension = dialog.getOpenFileName(None, title, working_directory, name_filter)
+        filepath, extension = dialog.getOpenFileName(parent, title, working_directory, name_filter)
         if filepath:
             callback(filepath)
 
