d11d60962452e8ffb9946950ed952a3b32418db9
MrFrangipane <contact@frangipane.me>
2026-01-19 18:40:48 +0100

    feat(CSS Editor): Render curly brackets variable values as multiple class selector

--- pyside6helpers/css/editor/project/css_renderer.py
+++ pyside6helpers/css/editor/project/css_renderer.py
@@ -10,17 +10,34 @@ class CSSRenderer:
         for section_name, template in project.templates.items():
             rendered_section = template
             for var_name, var_value in variables.items():
-                # Replace {{ var_name }} with var_value
-                # We use a simple regex for this
+
                 if var_value.startswith('[') and var_value.endswith(']'):
                     if len(var_value.split(',')) == 3:
                         var_value = f"rgb({var_value[1:-1]})"
                     elif len(var_value.split(',')) == 4:
                         var_value = f"rgba({var_value[1:-1]})"
-                pattern = r'\{\{\s*' + re.escape(var_name) + r'\s*\}\}'
-                rendered_section = re.sub(pattern, var_value, rendered_section)
+
+                if var_value.startswith('{') and var_value.endswith('}'):
+                    var_values = [var.strip() for var in var_value[1:-1].split(',')]
+                    pattern = r'(\{\{\s*' + var_name + r'\s*\}\}):?\s?([^{\s]+)?'
+
+                    def replace_func(match):
+                        full_match = match.group(0)
+                        selector = match.group(2)
+
+                        if selector:
+                            return ", ".join([f"{val}:{selector}" for val in var_values])
+                        return ", ".join(var_values)
+
+                    rendered_section = re.sub(pattern, replace_func, rendered_section)
+
+                else:
+                    pattern = r'\{\{\s*' + re.escape(var_name) + r'\s*\}\}'
+                    rendered_section = re.sub(pattern, var_value, rendered_section)
             
             full_css.append(f"/* Section: {section_name} */")
             full_css.append(rendered_section)
+
+        print("\n\n".join(full_css))
 
         return "\n\n".join(full_css)
