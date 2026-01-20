39c9404368711699ba12ba5d8a9210888dbf23af
MrFrangipane <contact@frangipane.me>
2026-01-20 14:46:32 +0100

    fix(CSS): use dark-desktop as default stylesheet

--- pyside6helpers/css/__init__.py
+++ pyside6helpers/css/__init__.py
@@ -3,7 +3,7 @@ from importlib import resources
 from PySide6.QtWidgets import QWidget, QApplication
 
 
-def load_onto(target: QWidget | QApplication, theme_filename: str = "dark.qss"):
+def load_onto(target: QWidget | QApplication, theme_filename: str = "dark-desktop.qss"):
     resource_path = resources.files("pyside6helpers.resources").joinpath(theme_filename)
     with resources.as_file(resource_path) as p:
         with open(p, "r") as f:
